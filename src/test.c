b2611982f8fb0f15c0251a8c926db801ec8a9650
Cosmin <cosmynteodr@yahoo.com>
2026-04-03 14:13:33 +0300

    Add: main modificat

--- src/test.c
+++ src/test.c
@@ -1,6 +1,6 @@
 #include "veclib1.h"
 
-int main(int argc,const* argv[]){
+int main(int argc,const char* argv[]){
 	FILE *fi=fopen(argv[1],"r");
 	FILE *fo=fopen(argv[2],"w");
 	sharpe_ratio(fi,fo);
