234d1456e9491e1052d0b7d9d555ecc0d03d2a9a
Cosmin Teodor <cosmynteodor@yahoo.com>
2026-05-13 23:01:22 +0300

    Am inclus structura tuturor functiilor in veclib1.h

--- veclib1.h
+++ veclib1.h
@@ -21,3 +21,9 @@ typedef struct queue{
 void Markov(FILE *fi, FILE *fo);
 void print_mat(float T[30][30], float nr_intervale);
 void read_obs(const float vals[30], float base, float dim_interv, float T[30][30], int nr_obs, int nr_intervale);
+void daily_prob(FILE *f, float T[30][30], float dim_interv, float p_target, int nr_interv, float base, float p_start, int nr_zile);
+prob *multyp_frac(prob *p, int neigb, int total);
+void add_frac(prob *prob_target, prob *p);
+void simplify(prob *p);
+int red(int a, int b);
+Node *enqueue(Node *node, int index, prob *p, int day);
